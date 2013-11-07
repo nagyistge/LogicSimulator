08b5ea31a52e366ca1dd183f08d588c79142f4a6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:19:15 +0100

    Added regions

--- LogicDemo/Model/Serializer.cs
+++ LogicDemo/Model/Serializer.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿#region References
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#endregion
+
 namespace Logic.Model
 {
-    class Serializer
-    {
-    }
+    #region Serializer
+
+
+
+    #endregion
 }
