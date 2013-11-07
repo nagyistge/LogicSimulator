3d5c1a30f2cb2debf42cd72dfde6dfa9c0e82007
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:18 +0100

    Added OrGate class

--- LogicDemo/Model/Gates/OrGate.cs
+++ LogicDemo/Model/Gates/OrGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region OrGate
 
+    public class OrGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public OrGate()
+            : base()
+        {
+        }
 
+        public OrGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("OrGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count < 2 ? false : !Inputs.All(i => i.State == false);
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
