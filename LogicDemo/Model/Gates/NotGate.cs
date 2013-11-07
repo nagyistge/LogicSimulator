97ede05916141967ab96a172df3f771f55aabf40
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:26 +0100

    Added NotGate class

--- LogicDemo/Model/Gates/NotGate.cs
+++ LogicDemo/Model/Gates/NotGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region NotGate
 
+    public class NotGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public NotGate()
+            : base()
+        {
+        }
 
+        public NotGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("NotGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count != 1 ? false : !Inputs.First().State;
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
