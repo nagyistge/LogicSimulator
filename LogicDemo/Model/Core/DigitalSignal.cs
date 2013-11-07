765a2e934da56e5a6fd11b4dcc42cfb7e6b7c548
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 20:50:56 +0100

    Updated DigitalSignal

--- LogicDemo/Model/Core/DigitalSignal.cs
+++ LogicDemo/Model/Core/DigitalSignal.cs
@@ -10,7 +10,69 @@ using System.Threading.Tasks;
 
 namespace Logic.Model.Core
 {
-    class DigitalSignal
+    #region DigitalSignal
+
+    public class     : LogicObject, IDigitalSignal
     {
+        #region Constructor
+
+        public DigitalSignal() { }
+
+        public DigitalSignal(bool state)
+            : this()
+        {
+            this.state = state;
+        }
+
+        #endregion
+
+        #region IDigitalSignal Implementation
+
+        private bool? state = null;
+        private DigitalPin inputPin = null;
+        private DigitalPin outputPin = null;
+
+        public bool? State
+        {
+            get { return state; }
+            set
+            {
+                if (value != state)
+                {
+                    state = value;
+                    Notify("State");
+                }
+            }
+        }
+
+        public DigitalPin InputPin
+        {
+            get { return inputPin; }
+            set
+            {
+                if (value != inputPin)
+                {
+                    inputPin = value;
+                    Notify("InputPin");
+                }
+            }
+        }
+
+        public DigitalPin OutputPin
+        {
+            get { return outputPin; }
+            set
+            {
+                if (value != outputPin)
+                {
+                    outputPin = value;
+                    Notify("OutputPin");
+                }
+            }
+        }
+
+        #endregion
     }
+
+    #endregion
 }
