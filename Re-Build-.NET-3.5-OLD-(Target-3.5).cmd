5279daafc0e03a8ac52cf84f16f0165ac36b1185
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:39:22 +0100

    Updated build scripts

--- Re-Build-.NET-3.5-OLD-(Target-3.5).cmd
+++ Re-Build-.NET-3.5-OLD-(Target-3.5).cmd
@@ -2,7 +2,7 @@
 
 set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
 
-MSBuild LogicDemo.sln /m /t:Rebuild /p:Configuration=Debug;TargetFrameworkVersion=v3.5
-MSBuild LogicDemo.sln /m /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5
+MSBuild Logic.sln /m /t:Rebuild /p:Configuration=Debug;TargetFrameworkVersion=v3.5
+MSBuild Logic.sln /m /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5
 
 pause
