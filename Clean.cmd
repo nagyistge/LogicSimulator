4b77c893b6dbae84b781771c9b74b13cdc656a67
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:47:57 +0100

    Removed old build scripts

--- Clean.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
-
-MSBuild Logic.sln /m /t:Clean /p:Configuration=Debug;TargetFrameworkVersion=v3.5
-MSBuild Logic.sln /m /t:Clean /p:Configuration=Release;TargetFrameworkVersion=v3.5
-
-pause
--- Re-Build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
-
-MSBuild Logic.sln /m /t:Rebuild /p:Configuration=Debug;TargetFrameworkVersion=v3.5
-MSBuild Logic.sln /m /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5
-
-pause
