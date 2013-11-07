5ef6e0099630c3a572be5d37fe01109c46f22726
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:54 +0100

    Added NorGate class

--- LogicDemo/Model/Gates/NorGate.cs
+++ LogicDemo/Model/Gates/NorGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region NorGate
 
+    public class NorGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public NorGate()
+            : base()
+        {
+        }
 
+        public NorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("NorGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count < 2 ? false : Inputs.All(i => i.State == false);
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
