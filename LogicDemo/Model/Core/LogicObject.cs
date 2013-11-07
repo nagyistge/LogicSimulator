7a9990bf87d1f7f4c148ff5dd46a5e15561b13ec
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 20:51:43 +0100

    Updated LogicObject

--- LogicDemo/Model/Core/LogicObject.cs
+++ LogicDemo/Model/Core/LogicObject.cs
@@ -10,7 +10,105 @@ using System.Threading.Tasks;
 
 namespace Logic.Model.Core
 {
-    class LogicObject
+    #region LogicObject
+
+    public abstract class LogicObject : INotifyPropertyChanged, ILogicObject, IId, ILocation
     {
+        #region INotifyPropertyChanged Implementation
+
+        public virtual void Notify(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region IId Implementation
+
+        private Guid id = Guid.Empty;
+        private string name = string.Empty;
+
+        public Guid Id
+        {
+            get { return id; }
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                    Notify("Id");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    Notify("Name");
+                }
+            }
+        }
+
+        #endregion
+
+        #region ILocation Implementation
+
+        private double x;
+        private double y;
+        private double z;
+
+        public double X
+        {
+            get { return x; }
+            set
+            {
+                if (value != x)
+                {
+                    x = value;
+                    Notify("X");
+                }
+            }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set
+            {
+                if (value != y)
+                {
+                    y = value;
+                    Notify("Y");
+                }
+            }
+        }
+
+        public double Z
+        {
+            get { return z; }
+            set
+            {
+                if (value != z)
+                {
+                    z = value;
+                    Notify("Z");
+                }
+            }
+        }
+
+        #endregion
     }
+
+    #endregion
 }
