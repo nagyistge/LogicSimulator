38170dfa8fc8cc1c01c985e440ab08000d59dd2b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 20:50:15 +0100

    Updated DigitalWire

--- LogicDemo/Model/Core/DigitalWire.cs
+++ LogicDemo/Model/Core/DigitalWire.cs
@@ -10,7 +10,57 @@ using System.Threading.Tasks;
 
 namespace Logic.Model.Core
 {
-    class DigitalWire
+    #region DigitalWire
+
+    public class DigitalWire : LogicObject, IDigitalWire
     {
+        #region IDigitalWire Implementation
+
+        private DigitalSignal signal;
+        private DigitalPin startPin;
+        private DigitalPin endPin;
+
+        public DigitalSignal Signal
+        {
+            get { return signal; }
+            set
+            {
+                if (value != signal)
+                {
+                    signal = value;
+                    Notify("Signal");
+                }
+            }
+        }
+
+        public DigitalPin StartPin
+        {
+            get { return startPin; }
+            set
+            {
+                if (value != startPin)
+                {
+                    startPin = value;
+                    Notify("StartPin");
+                }
+            }
+        }
+
+        public DigitalPin EndPin
+        {
+            get { return endPin; }
+            set
+            {
+                if (value != endPin)
+                {
+                    endPin = value;
+                    Notify("EndPin");
+                }
+            }
+        }
+
+        #endregion
     }
+
+    #endregion
 }
