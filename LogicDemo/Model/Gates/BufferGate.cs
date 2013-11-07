c5958d6d1dee9e0726118ec7995ff6e961eea0af
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:35 +0100

    Added BufferGate class

--- LogicDemo/Model/Gates/BufferGate.cs
+++ LogicDemo/Model/Gates/BufferGate.cs
@@ -12,7 +12,39 @@ namespace Logic.Model.Gates
 {
     #region BufferGate
 
+    public class BufferGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public BufferGate()
+            : base()
+        {
+        }
 
+        public BufferGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("BufferGate Calculate() (Name={0})", Name);
+
+            if (Inputs.Count > 0 && (Inputs.Count == Outputs.Count) && Inputs.All(i => i.State.HasValue))
+            {
+                for (int i = 0; i < Inputs.Count; i++)
+                {
+                    Outputs[i].State = Inputs[i].State;
+                }
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
