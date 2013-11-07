73b860b72ca24a3536870f885f0e5225538e7203
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:08:03 +0100

    Added XorGate class

--- LogicDemo/Model/Gates/XorGate.cs
+++ LogicDemo/Model/Gates/XorGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region XorGate
 
+    public class XorGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public XorGate()
+            : base()
+        {
+        }
 
+        public XorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("XorGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count < 2 ? false : Inputs.Count(i => i.State == true) % 2 != 0;
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
