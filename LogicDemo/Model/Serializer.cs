5977cd29f1c6bf76a91af3c2db208a032984a8f5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:20:36 +0100

    Added Serializer class

--- LogicDemo/Model/Serializer.cs
+++ LogicDemo/Model/Serializer.cs
@@ -12,7 +12,74 @@ namespace Logic.Model
 {
     #region Serializer
 
+    public static class Serializer
+    {
+        public static Type[] GetDiagramTypes()
+        {
+            return new Type[]
+            { 
+                typeof(DigitalPin),
+                typeof(DigitalSignal),
+                typeof(DigitalWire),
+                typeof(AndGate),
+                typeof(OrGate),
+                typeof(NotGate),
+                typeof(BufferGate),
+                typeof(NandGate),
+                typeof(NorGate),
+                typeof(XorGate),
+                typeof(XnorGate),
+                typeof(TimerPulse),
+                typeof(TimerOnDelay),
+                typeof(DigitalLogicDiagram)
+            };
+        }
 
+        public static DigitalLogicDiagram OpenDiagram()
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog()
+            {
+                DefaultExt = "xml",
+                Filter = "Xml Files (*.xml)|*.xml|All Files (*.*)|*.*",
+                FilterIndex = 0
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                var s = new DataContractSerializer(typeof(DigitalLogicDiagram), GetDiagramTypes(), int.MaxValue, true, true, null);
+
+                using (var reader = XmlReader.Create(dlg.FileName))
+                {
+                    return (DigitalLogicDiagram)s.ReadObject(reader);
+                }
+            }
+            return null;
+        }
+
+        public static void SaveDiagram(DigitalLogicDiagram diagram)
+        {
+            var dlg = new Microsoft.Win32.SaveFileDialog()
+            {
+                DefaultExt = "xml",
+                Filter = "Xml Files (*.xml)|*.xml|All Files (*.*)|*.*",
+                FilterIndex = 0,
+                FileName = "diagram1"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                if (diagram != null)
+                {
+                    var s = new DataContractSerializer(diagram.GetType(), GetDiagramTypes(), int.MaxValue, true, true, null);
+
+                    using (var writer = XmlWriter.Create(dlg.FileName, new XmlWriterSettings() { Indent = true, IndentChars = "    " }))
+                    {
+                        s.WriteObject(writer, diagram);
+                    }
+                }
+            }
+        }
+    }
 
     #endregion
 }
