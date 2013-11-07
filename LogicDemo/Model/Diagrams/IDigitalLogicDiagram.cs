70a9bc3d975720c6c807445f1202803777ab8006
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:17:44 +0100

    Added missing References

--- LogicDemo/Model/Diagrams/IDigitalLogicDiagram.cs
+++ LogicDemo/Model/Diagrams/IDigitalLogicDiagram.cs
@@ -1,7 +1,9 @@
 ﻿#region References
 
+using Logic.Model.Core;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
