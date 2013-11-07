65515d7d77155d35360924156966b13256a3bf92
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:06 +0100

    Added AndGate class

--- LogicDemo/Model/Gates/AndGate.cs
+++ LogicDemo/Model/Gates/AndGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region AndGate
 
+    public class AndGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public AndGate()
+            : base()
+        {
+        }
 
+        public AndGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("AndGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = (Inputs.Count < 2) ? false : Inputs.All(i => (bool)i.State == true);
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
