7f39429fa826373c017aecc3b9311ef5c7d3359a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 20:15:14 +0100

    Added References region

--- LogicDemo/App.xaml.cs
+++ LogicDemo/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿#region References
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Windows;
+using System.Windows; 
+
+#endregion
 
 namespace WpfApplication1
 {
