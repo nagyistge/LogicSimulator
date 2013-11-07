6ba64c88fe476595c4d2f5c9d6c34f86b70cec6f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:16:24 +0100

    Added DigitalLogicDiagram class

--- LogicDemo/Model/Diagrams/DigitalLogicDiagram.cs
+++ LogicDemo/Model/Diagrams/DigitalLogicDiagram.cs
@@ -12,7 +12,52 @@ namespace Logic.Model.Diagrams
 {
     #region DigitalLogicDiagram
 
- 
+    public class DigitalLogicDiagram : DigitalLogic, IDigitalLogicDiagram
+    {
+        #region Constructor
+
+        /*
+        public DigitalLogicDiagram()
+            : base()
+        {
+        }
+
+        public DigitalLogicDiagram(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+
+        #endregion
+
+        #region IDigitalLogicDiagram Implementation
+
+        private ObservableCollection<LogicObject> elements = new ObservableCollection<LogicObject>();
+
+        public ObservableCollection<LogicObject> Elements
+        {
+            get
+            {
+                return elements;
+            }
+            set
+            {
+                if (value != elements)
+                {
+                    elements = value;
+                    Notify("Elements");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate() { }
+
+        #endregion
+    }
 
     #endregion
 }
