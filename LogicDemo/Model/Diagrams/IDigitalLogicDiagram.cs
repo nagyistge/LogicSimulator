b4c2bd53311d72701abd601bcb58d1dc6caf629c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:15:43 +0100

    Added regions

--- LogicDemo/Model/Diagrams/IDigitalLogicDiagram.cs
+++ LogicDemo/Model/Diagrams/IDigitalLogicDiagram.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿#region References
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#endregion
+
 namespace Logic.Model.Diagrams
 {
-    class IDigitalLogicDiagram
-    {
-    }
+    #region IDigitalLogicDiagram
+
+
+
+    #endregion
 }
