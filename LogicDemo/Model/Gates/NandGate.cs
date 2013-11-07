8614414420bd126c64e4fd596d991fb769c03fe5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:07:44 +0100

    Added NandGate class

--- LogicDemo/Model/Gates/NandGate.cs
+++ LogicDemo/Model/Gates/NandGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region NandGate
 
+    public class NandGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public NandGate()
+            : base()
+        {
+        }
 
+        public NandGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("NandGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count < 2 ? false : !Inputs.All(i => i.State == true);
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
