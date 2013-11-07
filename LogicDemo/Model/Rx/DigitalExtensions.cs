61ae8822b1fc3c3a8a14cc52f93d7537f3279747
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 20:59:08 +0100

    Added missing References

--- LogicDemo/Model/Rx/DigitalExtensions.cs
+++ LogicDemo/Model/Rx/DigitalExtensions.cs
@@ -1,8 +1,10 @@
 ﻿#region References
 
+using Logic.Model.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
 using System.Text;
 using System.Threading.Tasks; 
 
