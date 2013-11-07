786da5ba38a20264a0a327239989ffa1bbabe407
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-07 21:08:13 +0100

    Added XnorGate class

--- LogicDemo/Model/Gates/XnorGate.cs
+++ LogicDemo/Model/Gates/XnorGate.cs
@@ -12,7 +12,36 @@ namespace Logic.Model.Gates
 {
     #region XnorGate
 
+    public class XnorGate : DigitalLogic
+    {
+        #region Constructor
+        /*
+        public XnorGate()
+            : base()
+        {
+        }
 
+        public XnorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
+            : base(collectionScheduler, simulationScheduler)
+        {
+        }
+        */
+        #endregion
+
+        #region Calculate Implementation
+
+        public override void Calculate()
+        {
+            //System.Diagnostics.Debug.Print("XnorGate Calculate() (Name={0})", Name);
+
+            if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
+            {
+                Outputs.First().State = Inputs.Count < 2 ? false : Inputs.Count(i => i.State == true) % 2 == 0;
+            }
+        }
+
+        #endregion
+    }
 
     #endregion
 }
