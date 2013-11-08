4e0b41d7f30efc30237a536d6f72b5c8aae59d2c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:09:31 +0100

    Code cleanup

--- LogicDemo/Model/Gates/AndGate.cs
+++ LogicDemo/Model/Gates/AndGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class AndGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public AndGate()
-            : base()
-        {
-        }
-
-        public AndGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("AndGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = (Inputs.Count < 2) ? false : Inputs.All(i => (bool)i.State == true);
