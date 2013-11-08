78a2a7a18903f045347be7a5a3d7167bbb918ef4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:07:57 +0100

    Code cleanup

--- LogicDemo/Model/Gates/XorGate.cs
+++ LogicDemo/Model/Gates/XorGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class XorGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public XorGate()
-            : base()
-        {
-        }
-
-        public XorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("XorGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count < 2 ? false : Inputs.Count(i => i.State == true) % 2 != 0;
