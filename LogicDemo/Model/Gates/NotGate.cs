ef17d402f0823c5b6754882fcf149fb538f8fe0c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:08:45 +0100

    Code cleanup

--- LogicDemo/Model/Gates/NotGate.cs
+++ LogicDemo/Model/Gates/NotGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class NotGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public NotGate()
-            : base()
-        {
-        }
-
-        public NotGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("NotGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count != 1 ? false : !Inputs.First().State;
