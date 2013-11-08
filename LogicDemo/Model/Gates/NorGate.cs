35700496585f18721278fec3fb2eaf5a77d7832b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:08:56 +0100

    Code cleanup

--- LogicDemo/Model/Gates/NorGate.cs
+++ LogicDemo/Model/Gates/NorGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class NorGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public NorGate()
-            : base()
-        {
-        }
-
-        public NorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("NorGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count < 2 ? false : Inputs.All(i => i.State == false);
