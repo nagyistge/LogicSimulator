0e98617290942f8282aed7b2f9caa7bcba6f2ac6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:08:31 +0100

    Code cleanup

--- LogicDemo/Model/Gates/OrGate.cs
+++ LogicDemo/Model/Gates/OrGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class OrGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public OrGate()
-            : base()
-        {
-        }
-
-        public OrGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("OrGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count < 2 ? false : !Inputs.All(i => i.State == false);
