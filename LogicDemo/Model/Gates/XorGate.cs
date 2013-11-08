1e4316e29a1fbc73346d3e94226ea297cf1c1086
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:08:04 +0100

    Code cleanup

--- LogicDemo/Model/Gates/XorGate.cs
+++ LogicDemo/Model/Gates/XorGate.cs
@@ -18,7 +18,7 @@ namespace Logic.Model.Gates
         #region Calculate Implementation
 
         public override void Calculate()
-        {
+        {   
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count < 2 ? false : Inputs.Count(i => i.State == true) % 2 != 0;
