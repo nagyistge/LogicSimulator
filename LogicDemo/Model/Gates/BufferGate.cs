a786a342c26f9eb043b27e2f7f8f8f37f8215b57
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:09:18 +0100

    Code cleanup

--- LogicDemo/Model/Gates/BufferGate.cs
+++ LogicDemo/Model/Gates/BufferGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class BufferGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public BufferGate()
-            : base()
-        {
-        }
-
-        public BufferGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("BufferGate Calculate() (Name={0})", Name);
-
             if (Inputs.Count > 0 && (Inputs.Count == Outputs.Count) && Inputs.All(i => i.State.HasValue))
             {
                 for (int i = 0; i < Inputs.Count; i++)
