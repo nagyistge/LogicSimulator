e74049b1b21e3f75f8c8da4e0df7b5e9b889416e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-11-08 22:08:20 +0100

    Code cleanup

--- LogicDemo/Model/Gates/XnorGate.cs
+++ LogicDemo/Model/Gates/XnorGate.cs
@@ -15,26 +15,10 @@ namespace Logic.Model.Gates
 
     public class XnorGate : DigitalLogic
     {
-        #region Constructor
-        /*
-        public XnorGate()
-            : base()
-        {
-        }
-
-        public XnorGate(IScheduler collectionScheduler, IScheduler simulationScheduler)
-            : base(collectionScheduler, simulationScheduler)
-        {
-        }
-        */
-        #endregion
-
         #region Calculate Implementation
 
         public override void Calculate()
         {
-            //System.Diagnostics.Debug.Print("XnorGate Calculate() (Name={0})", Name);
-
             if (Outputs.Count == 1 && Inputs.All(i => i.State.HasValue))
             {
                 Outputs.First().State = Inputs.Count < 2 ? false : Inputs.Count(i => i.State == true) % 2 == 0;
